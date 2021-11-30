78a875d667a84725c3a6350597948370f4a684ac
Gabriel Rodrigues <Gabers357@gmail.com>
2021-11-29 21:04:14 -0300

    alterada a estrutura de pastas

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const input1 = document.getElementById('num1') as HTMLInputElement;
 const input2 = document.getElementById('num2')as HTMLInputElement;
-const button = document.getElementById('somar');
+const button = document.getElementById('somar')!;
 
 
 function sum(a:number,b:number) {
